c5c23a14a0eef75901e1aefa7f2fec0018f5d2cb
susanket <susanket@aereo.io>
2025-03-31 08:51:19 +0530

    Refactor project structure: remove unused code and implement new functions for loading CSV and plotting feature distributions

--- dummy-project/src/dataset.py
+++ dummy-project/src/dataset.py
@@ -1,29 +1,5 @@
-from pathlib import Path
+import pandas as pd
 
-from loguru import logger
-from tqdm import tqdm
-import typer
-
-from dummy_project.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
-
-app = typer.Typer()
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
-
-
-if __name__ == "__main__":
-    app()
+def load_csv(path: str) -> pd.DataFrame:
+    """Loads a CSV file from the given path."""
+    return pd.read_csv(path)
